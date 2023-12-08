9e3935e6a1c8bea6d1295b7a338b44de98c41fc3
yasin <yasinjbeli710@gmail.com>
2023-12-08 16:20:58 +0100

    added even more shi

--- tutor_portal/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Course
-
-class CourseForm(forms.ModelForm):
-    class Meta:
-        model = Course
-        fields = ['title', 'description', 'enrollment_capacity', 'tutor']
-        # Add widgets and other configurations if needed
